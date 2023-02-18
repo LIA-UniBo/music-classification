dabbfc38d0e6ac74d87a5b17300487e696317905
Alessandro Stockman <alessandrostockman@gmail.com>
2023-02-18 16:42:53 +0100

    add eval function

---
+++ src/train.py
@@ -0,0 +1,12 @@
+import evaluate
+import numpy as np
+
+
+def get_metrics_func():
+    accuracy = evaluate.load("accuracy")
+
+    def compute_metrics(eval_pred):
+        predictions = np.argmax(eval_pred.predictions, axis=1)
+        return accuracy.compute(predictions=predictions, references=eval_pred.label_ids)
+
+    return compute_metrics

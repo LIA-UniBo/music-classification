4c4e03be970cfde8e565307e01ad9180ad1c579c
Alessandro Stockman <alessandrostockman@gmail.com>
2023-02-18 14:04:16 +0100

    update dataset utils

--- src/dataset.py
+++ src/dataset.py
@@ -32,8 +32,7 @@ DATASET_FEATURES = [
 ]
 
 
-# Functions
-def filter_df(df, keep_features, audios_dir_path):
+def filter_df(df, keep_features, audios_dir_path, top_n=None, samples=None):
     drop_features = [f for f in DATASET_FEATURES if f not in keep_features]
     print(f"Only {len(DATASET_FEATURES) - len(drop_features)} features considered")
 
@@ -50,6 +49,20 @@ def filter_df(df, keep_features, audios_dir_path):
     df = df[~df_nans]
     print(f"Considering only rows without NaNs, {df_nans.sum()} samples discarded")
     print(f"{len(df)} samples left")
+
+    if top_n:
+        for f, n in top_n.items():
+            # TODO: What happens with multiple top_n?
+            df = df[df[f].isin(df.value_counts(f, sort=True)[:n].index)]
+            print(f"Keeping only the {n} most frequent values of {f}")
+            print(f"{len(df)} samples left")
+
+    if samples:
+        # TODO: What should happen with multiple keep_features?
+        df = sample_df(df, keep_features[0], total_samples=samples)
+        print("Applying stratified sampling to the database")
+        print(f"{len(df)} samples left")
+
     return df
 
 
@@ -76,22 +89,25 @@ def get_features_dict(df, dataset_features):
 
 def get_dataset(df, dataset_features=DATASET_FEATURES):
     features = Features.from_dict(get_features_dict(df, dataset_features))
-    ds = Dataset.from_pandas(df.reset_index(drop=True), features=features)
-    ds = ds.add_column(name="audio", column=df["mp3_path"])
-    ds = ds.cast_column(
-        "audio",
-        Audio(
-            # sampling_rate=16_000 #TODO: Depends on the model
-        ),
-    )
+    ds = Dataset.from_pandas(
+        df.reset_index(drop=True), features=features
+    )  # TODO: Save as strings and convert to ClassLabels only in "prepare_df" - how to keep same ids?
+    ds = ds.rename_column("mp3_path", "audio_path")
+    # ds = ds.add_column(name="audio", column=df["mp3_path"])
+    # ds = ds.cast_column("audio", Audio(
+    #     # sampling_rate=16_000 #TODO: Depends on the model
+    # ))
 
+    return ds
+
+
+def get_label_mappings(ds):
     label_maps = {}
     for f in ds.features:
         if type(ds.features[f]) == datasets.features.ClassLabel:
             m = create_label_maps(ds.features[f])
             label_maps[f] = {"l2i": m[0], "i2l": m[1]}
-
-    return ds, label_maps
+    return label_maps
 
 
 def sample_df(df, target_feature, total_samples):

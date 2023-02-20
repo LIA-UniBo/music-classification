1f821d3231d01f3ecd05cb5bcae410e2ce362f4e
Alessandro Stockman <alessandrostockman@gmail.com>
2023-02-20 16:42:49 +0100

    test with data collator

--- src/train.py
+++ src/train.py
@@ -1,4 +1,10 @@
 import os
+import random
+import warnings
+from collections.abc import Mapping
+from dataclasses import dataclass
+from random import randint
+from typing import Any, Callable, Dict, List, NewType, Optional, Tuple, Union
 
 import evaluate
 import numpy as np
@@ -9,6 +15,7 @@ from transformers import (
     Trainer,
     TrainingArguments,
 )
+from transformers.utils import PaddingStrategy
 
 from src.dataset import (
     FEATURE_ENCODER_TO_HF_HUB,
@@ -20,6 +27,50 @@ PROJECT_NAME = "music-classification-aii"
 MAX_AUDIO_LEN_S = (
     10  # TODO: Transformers are O(n^2) so high audio len could be prohibitive
 )
+
+
+# @dataclass
+# class DataCollatorWithPadding:
+#     """
+#     Data collator that will dynamically pad the inputs received.
+
+#     Args:
+#         tokenizer ([`PreTrainedTokenizer`] or [`PreTrainedTokenizerFast`]):
+#             The tokenizer used for encoding the data.
+#         padding (`bool`, `str` or [`~utils.PaddingStrategy`], *optional*, defaults to `True`):
+#             Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
+#             among:
+
+#             - `True` or `'longest'` (default): Pad to the longest sequence in the batch (or no padding if only a single
+#               sequence is provided).
+#             - `'max_length'`: Pad to a maximum length specified with the argument `max_length` or to the maximum
+#               acceptable input length for the model if that argument is not provided.
+#             - `False` or `'do_not_pad'`: No padding (i.e., can output a batch with sequences of different lengths).
+#         max_length (`int`, *optional*):
+#             Maximum length of the returned list and optionally padding length (see above).
+#         pad_to_multiple_of (`int`, *optional*):
+#             If set will pad the sequence to a multiple of the provided value.
+
+#             This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
+#             7.5 (Volta).
+#         return_tensors (`str`):
+#             The type of Tensor to return. Allowable values are "np", "pt" and "tf".
+#     """
+
+#     padding: Union[bool, str, PaddingStrategy] = True
+#     max_length: Optional[int] = None
+#     pad_to_multiple_of: Optional[int] = None
+#     return_tensors: str = "pt"
+
+#     def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
+#         batch = self.tokenizer.pad(
+#             features,
+#             padding=self.padding,
+#             max_length=self.max_length,
+#             pad_to_multiple_of=self.pad_to_multiple_of,
+#             return_tensors=self.return_tensors,
+#         )
+#         return batch
 
 
 def get_preprocess_func(training_config):
@@ -87,13 +138,16 @@ def get_trainer(
     train_batch_size = training_config["train_batch_size"]
     eval_batch_size = training_config["eval_batch_size"]
 
+    if feature_extractor is None:
+        feature_extractor = get_feature_extractor()
+
     wandb.init(
         project=PROJECT_NAME,
         name=run_name,
         tags=([env] if env else []) + (["debug"] if debug else []),
     )
 
-    data_collator = DataCollatorWithPadding()
+    data_collator = DataCollatorWithPadding(tokenizer=feature_extractor)
 
     training_args = TrainingArguments(
         run_name=run_name,

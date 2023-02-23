c4b5b09a3368b211e8794ee99f534b98ebf8fbbd
Alessandro Stockman <alessandrostockman@gmail.com>
2023-02-23 17:09:22 +0100

    add evaluator

--- src/train.py
+++ src/train.py
@@ -1,9 +1,12 @@
+import math
 import os
+import time
 
 import evaluate
 import numpy as np
 import wandb
 from transformers import AutoConfig, DataCollatorWithPadding, Trainer, TrainingArguments
+from transformers.trainer_utils import speed_metrics
 
 from src.dataset import FEATURE_ENCODER_DETAILS, get_feature_extractor
 from src.utils import get_feature_label_mapping
@@ -59,10 +62,16 @@ def get_model(training_config, ds):
     config.label2id = l2i
     config.id2label = i2l
 
-    model = fe_details["class"].from_pretrained(
-        fe_details["pretrained"],
-        config=config,
-    )
+    if "model_path" in training_config:
+        model = fe_details["class"].from_pretrained(
+            training_config["model_path"],
+            config=config,
+        )
+    else:
+        model = fe_details["class"].from_pretrained(
+            fe_details["pretrained"],
+            config=config,
+        )
 
     if training_config["freeze_encoder"]:
         model.freeze_feature_encoder()
@@ -80,6 +89,7 @@ def get_trainer(
     output_dir="out",
     debug=False,
     env=None,
+    report_to="all",
 ):
     epochs = training_config["epochs"]
     train_batch_size = training_config["train_batch_size"]
@@ -90,11 +100,12 @@ def get_trainer(
     if feature_extractor is None:
         feature_extractor = get_feature_extractor(training_config=training_config)
 
-    wandb.init(
-        project=PROJECT_NAME,
-        name=run_name,
-        tags=([env] if env else []) + (["debug"] if debug else []),
-    )
+    if report_to:
+        wandb.init(
+            project=PROJECT_NAME,
+            name=run_name,
+            tags=([env] if env else []) + (["debug"] if debug else []),
+        )
 
     data_collator = DataCollatorWithPadding(tokenizer=feature_extractor)
 
@@ -113,7 +124,7 @@ def get_trainer(
         metric_for_best_model="accuracy",
         greater_is_better=True,
         save_total_limit=3,
-        report_to="all",
+        report_to=report_to,
         logging_steps=50,
         logging_first_step=True,
     )
@@ -131,6 +142,92 @@ def get_trainer(
     return trainer
 
 
+def get_evaluator(
+    model,
+    training_config,
+    feature_extractor=None,
+    output_dir="out",
+):
+    epochs = training_config["epochs"]
+    train_batch_size = training_config["train_batch_size"]
+    eval_batch_size = training_config["eval_batch_size"]
+
+    if feature_extractor is None:
+        feature_extractor = get_feature_extractor(training_config=training_config)
+
+    data_collator = DataCollatorWithPadding(tokenizer=feature_extractor)
+
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        num_train_epochs=epochs,
+        per_device_train_batch_size=train_batch_size,
+        per_device_eval_batch_size=eval_batch_size,
+    )
+
+    trainer = EvaluatorTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=None,
+        eval_dataset=None,
+        tokenizer=feature_extractor,
+        data_collator=data_collator,
+        compute_metrics=get_metrics_func(),
+    )
+
+    return trainer
+
+
 def end_training(run_name, trainer, models_dir_path):
     wandb.finish()
     trainer.save_model(os.path.join(models_dir_path, run_name))
+
+
+class EvaluatorTrainer(Trainer):
+    def evaluate(
+        self,
+        eval_dataset=None,
+        ignore_keys=None,
+        metric_key_prefix="eval",
+    ):
+        # memory metrics - must set up as early as possible
+        self._memory_tracker.start()
+
+        eval_dataloader = self.get_eval_dataloader(eval_dataset)
+        start_time = time.time()
+
+        eval_loop = (
+            self.prediction_loop
+            if self.args.use_legacy_prediction_loop
+            else self.evaluation_loop
+        )
+        output = eval_loop(
+            eval_dataloader,
+            description="Evaluation",
+            # No point gathering the predictions if there are no metrics, otherwise we defer to
+            # self.args.prediction_loss_only
+            prediction_loss_only=True if self.compute_metrics is None else None,
+            ignore_keys=ignore_keys,
+            metric_key_prefix=metric_key_prefix,
+        )
+
+        total_batch_size = self.args.eval_batch_size * self.args.world_size
+        if f"{metric_key_prefix}_jit_compilation_time" in output.metrics:
+            start_time += output.metrics[f"{metric_key_prefix}_jit_compilation_time"]
+        output.metrics.update(
+            speed_metrics(
+                metric_key_prefix,
+                start_time,
+                num_samples=output.num_samples,
+                num_steps=math.ceil(output.num_samples / total_batch_size),
+            )
+        )
+
+        self.log(output.metrics)
+
+        self.control = self.callback_handler.on_evaluate(
+            self.args, self.state, self.control, output.metrics
+        )
+
+        self._memory_tracker.stop_and_update_metrics(output.metrics)
+
+        return output
